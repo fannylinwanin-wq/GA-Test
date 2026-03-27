4819f7f4ad59855421cef9f1d0e55091518e133b
fanny.lin <>
2026-03-27 16:41:11 +0800

    feat:新增client_id

--- index.a7ec5.js
+++ index.a7ec5.js
@@ -1,4 +1,4 @@
-System.register(["./application.37f3c.js"], function (_export, _context) {
+System.register(["./application.b6424.js"], function (_export, _context) {
   "use strict";
 
   var Application, application;
